4b1d00fb42760b950d77b8277e7606145dbb0d4d
nihaxmuhd <muhammadnihad16@gmail.com>
2026-06-11 01:16:23 +0530

    Remove create admin endpoint references

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,8 +4,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializers import LoginSerializer, UserSerializer
