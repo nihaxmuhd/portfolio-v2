1f78e7f3901170580e25b5be00358aeaa4300915
nihaxmuhd <muhammadnihad16@gmail.com>
2026-06-12 23:52:50 +0530

    fix create_admin import error

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-
 from . import views
-from .views import create_admin
 
 app_name = "accounts"
 
@@ -9,6 +7,5 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("me/", views.MeView.as_view(), name="me"),
-
     path("reset-users/", views.reset_users, name="reset-users"),
 ]
